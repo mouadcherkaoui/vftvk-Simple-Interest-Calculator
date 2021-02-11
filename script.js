f019d5aaf4bda0ef1e1614a7435476b555748c03
cherkaoui mouad <cherkaoui.mouad@live.fr>
2021-02-11 16:50:33 +0100

    Fix

--- script.js
+++ script.js
@@ -9,28 +9,28 @@ function compute()
 
     var resultElem = document.getElementById("result");
 
-    // check params
     if(parseFloat(principal) <= 0 || principal == "") {
         alert("Enter a positive number");
         document.getElementById("principal").focus();
     }
-    // calculate interest
     else {
         var interest = principal * years * rate / 100;
         var temporary = (new Date()).getFullYear();
         temporary = temporary + parseInt(years);
         
-        applyResult(resultElem, principal, interest, rate, temporary);
+        resultElem.innerHTML= `
+        <div class='result'> 
+            <ul> 
+                <li>If you deposit ${principal} </li> 
+                <li>at an interest rate of ${rate}%. </li>
+                <li>You will receive an amount of ${interest} </li>
+                <li>in the year ${temporary}</li>
+            </ul>
+        </div>`;
     }
 }
 
-function applyResult(elem, principal, interest, rate, years) {
-    elem.innerHtml= "<div class='result'> <ul> <li>If you deposit " + principal + "</li> <li>at an interest rate of " + rate + "%.</li><li>You will receive an amount of " + interest + ",</li><li>in the year " + years + "</li></ul></div>";
-}
-
 // update rate label function
-function updateRateLabel() {
-    rate = document.getElementById("rate").value;
-
-    document.getElementById("rateLabel").innerHTML = rate + " %";
+function updateRate() {
+    document.getElementById("rate-value").innerHTML = `${document.getElementById("rate").value * 10}%`;
 }
